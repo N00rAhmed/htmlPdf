ded14be8bd9cd4d0c36a0a111fa54c4cb61f3814
William Jefferson Mensah <88050600+cuuj69@users.noreply.github.com>
2024-02-04 20:03:47 +0000

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from translation_api import TranslationAPI
 from gui import TranslationGUI
 
 if __name__ == "__main__":
-    base_url = 'http://localhost:5000'
+    base_url = 'localhost'
     api = TranslationAPI(base_url)
     supported_languages = api.fetch_supported_languages()
 
